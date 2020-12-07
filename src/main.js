e7a1334789620f05b43cdbc91c75024feda92f9a
edianibarrola <edianibarrola@gmail.com>
2020-12-07 21:01:33 +0000

    initial commit

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+// let myFunction = () => {};
+// document.querySelector("#inputCC").addEventListener("click", myFunction);
+
+//document.getElementById("sendButton").addEventListener("click", myFunction);
+
+console.log(document.forms);
